c6552e03862b8675130ed5d8c825403066d58d9c
Taria Reese <118138394+tariangelita2420@users.noreply.github.com>
2022-11-16 18:45:33 +0000

    We added a function with our names and something we learned about git

---
+++ .ccls-cache/@home@runner@lab-13-1/211-f22-collab-github@collab.c
@@ -0,0 +1,36 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void reed_ekidan2() {
+  printf("Dale Reed and Ellen Kidane\n");
+  printf("We learned that git and GitHub are amazing, powerful tools!\n");
+}
+
+void treese3_sbijo2(){
+  printf("Taria Reese and Sidharth Bijo");
+  printf("We learned how using git can allow to collabertate on code using git commands such as git log and git log allows you to see the history of all commited changes");
+}
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    reed_ekidan2();
+    
+    return 0;
+}
